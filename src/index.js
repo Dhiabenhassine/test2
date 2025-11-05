943bbea472f938d6d11aaa2d1ec53ae2cd1f1a07
Dhiabenhassine <dhiabenhssine123@gmail.com>
2025-11-05 18:50:48 +0000

    version 0.1.7

--- src/index.js
+++ src/index.js
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// ✅ import these from Material UI
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-// ✅ create a default or custom theme
-const theme = createTheme();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* ✅ wrap App inside ThemeProvider */}
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
-
 reportWebVitals();
